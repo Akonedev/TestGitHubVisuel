e165d2eb46ce93f62cea6c17a78c65c5f0846c7f
akonedev <akonedev@gmail.com>
2022-04-06 16:35:26 +0200

    Ajout class1 commentaire

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitHubVisuel
+{
+    internal class Class1
+    {
+    }
+}
